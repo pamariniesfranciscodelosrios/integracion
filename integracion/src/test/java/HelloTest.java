729bb6f53f37793e89079aab84bb812d4a417322
pamariniesfranciscodelosrios <pamarin@iesfranciscodelosrios.es>
2025-05-14 08:07:23 +0200

    inicial

---
+++ integracion/src/test/java/HelloTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+    @Test
+    void testHello() {
+        assertEquals(2, 1 + 1);
+    }
+}
